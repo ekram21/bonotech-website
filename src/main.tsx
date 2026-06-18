e84fc81324038e592f60691cc4677d4360e1e47f
Ekram <ekram_lol@hotmail.com>
2026-06-18 13:28:41 +0600

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@/styles/globals.css'
 import App from './App.tsx'
 
+//root component
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
